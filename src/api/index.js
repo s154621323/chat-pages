07cc9efdc3ebbe539b712c07906706c1c99ba9be
sun-zx <154621323@qq.com>
2025-05-11 17:12:06 +0800

    更改接口地址

--- src/api/index.js
+++ src/api/index.js
@@ -7,7 +7,7 @@ import { graphqlRequest } from '../utils/http';
 // API 配置
 const API_CONFIG = {
   // GraphQL API 端点
-  GRAPHQL_URL: 'http://localhost:8787/graphql',
+  GRAPHQL_URL: 'https://chat-workers.edensun0609.workers.dev/graphql',
 
   // 可以在这里添加其他 API 配置，如鉴权令牌等
 };
